5dceb1440ba5398da0d0e3b0191e3eaa7ca33c8a
Zai Flux <rodhelcondicion@gmail.com>
2026-07-29 13:43:11 +0800

    turn the add files btn display a file

--- 02-CloudHaux/frontend/static/js/dashboard.js
+++ 02-CloudHaux/frontend/static/js/dashboard.js
@@ -1,10 +1,10 @@
 const addFileBtn = document.getElementById("addFileBtn");
 const fileInput = document.getElementById("fileInput");
+const fileList = document.getElementById("fileList");
 
 addFileBtn.addEventListener("click", () => {
   fileInput.click();
 });
-
 
 fileInput.addEventListener("change", () => {
   const files = fileInput.files;
@@ -13,4 +13,15 @@ fileInput.addEventListener("change", () => {
     return;
   }
 
+  for (const file of files) {
+    const fileCard = document.createElement("div");
+    fileCard.className = "file_card";
+
+    fileCard.innerHTML = `
+            <h3>${file.name}</h3>
+            <p>${(file.size / 1024).toFixed(2)} KB</p>
+        `;
+
+    fileList.appendChild(fileCard);
+  }
 });
